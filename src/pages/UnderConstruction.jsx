552d6674ec4eea26fd53fba0572ff030115c02bb
Rada749 <radames5897@gmail.com>
2024-12-17 15:14:45 +0100

    add underconstruction page

--- src/pages/UnderConstruction.jsx
+++ src/pages/UnderConstruction.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const UnderConstruction = () => {
   return (
-    <div>
-      <p>Página en construccion :D</p>
+    <div className="bg-[#1e1e2d] text-white min-h-screen flex flex-col mt-[4.5rem]">
+      <div className="bg-gray-800 text-gray-400 shadow-md rounded-lg p-6">
+        <h2 className="text-3xl font-bold text-center">Estamos trabajando para ofrecerte una experiencia completa!</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UnderConstruction
+export default UnderConstruction;
